66208ab437c2f7bcd6a155fb3e094b2a8ef289ae
E.Wolf <e.wolf@webmi.ru>
2024-01-26 01:06:19 +0300

    Bug fix & improvements

--- http/config/src/Index.vue
+++ http/config/src/Index.vue
@@ -37,11 +37,69 @@ export default {
 
 <template>
   <header>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        </a>
 
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Home
+          </a>
+
+          <a class="navbar-item">
+            Documentation
+          </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+<!--          <div class="navbar-item">-->
+<!--            <label class="checkbox">-->
+<!--              <input type="checkbox"> Режим эксперта</label>-->
+<!--          </div>-->
+          <div class="navbar-item">
+            <div class="buttons">
+              <button @click="configPost" class="button is-success">Сохранить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <div class="columns">
-  <aside class="menu column is-4">
+  <aside class="menu column is-4" style="padding: 30px">
     <p class="menu-label">
       Чаты
     </p>
@@ -61,8 +119,7 @@ export default {
     </ul>
 
   </aside>
-  <main class="column is-8">
-    <button @click="configPost" class="button is-success">Сохранить</button>
+  <main class="column is-8" style="padding: 30px">
     <router-view></router-view>
   </main>
   </div>
